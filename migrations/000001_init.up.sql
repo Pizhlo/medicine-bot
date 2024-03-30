7267e98be7557c46f57c06ea882c1c3daf23b2e4
Maria <zaush@bk.ru>
2024-03-30 09:52:07 +0300

    added migrations; added makefile

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA IF NOT EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users.users(
+    id serial not null,
+    tg_id int not null,
+    primary key(id)
+);
+
+CREATE SCHEMA IF NOT EXISTS drugs;
+
+CREATE TABLE IF NOT EXISTS drugs.drugs(
+    id serial not null,
+    user_id int not null,
+    name text not null,
+    desription text,
+    created timestamp not null,
+    take_today bool not null default false,
+    today_count int not null default 0,
+    primary key(id),
+    foreign key (user_id) references users.users(id) on delete cascade
+)
